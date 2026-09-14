a75531156c5890fb0acb924da5c306757a5d5436
parkashrawal <147302763+parkashrawal@users.noreply.github.com>
2026-09-14 14:51:32 +0530

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'number-mela-v1';
+const urlsToCache = ['/', '/index.html', '/style.css', '/script.js'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => {
+      return Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) return caches.delete(name);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
